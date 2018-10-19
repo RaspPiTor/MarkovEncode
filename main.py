ceebf12ee970cfb68c59ae57a8140af2eaec2f71
Alex Williams <hughaw64@gmail.com>
2018-10-19 07:53:16 +0100

    Added GUI, and begun reducing size of output

---
+++ main.py
@@ -0,0 +1,44 @@
+import markov
+import tkinter.ttk as ttk
+import tkinter as tk
+class CompleteText(tk.Text):
+    def __init__(self, *args, **kwargs):
+        tk.Text.__init__(self, *args, **kwargs, undo=True)
+        self.bind('<Control-Key-a>', self.event_select_all)
+        self.bind('<Control-Key-A>', self.event_select_all)
+    def event_select_all(self, event):
+        self.tag_add(tk.SEL, "1.0", tk.END)
+        self.mark_set(tk.INSERT, "1.0")
+        self.see(tk.INSERT)
+        return 'break'
+class GUI(ttk.Frame):
+    def __init__(self,master=None):
+        ttk.Frame.__init__(self)
+        self.me = markov.MarkovEncode(markov.shakespeare())
+        ttk.Label(self, text='Plaintext:').grid(row=0, column=0)
+        self.plain_in = CompleteText(self, width=80, height=8)
+        self.plain_in.grid(row=1, column=0)
+        self.encoded_out = CompleteText(self)
+        self.encoded_out.grid(row=2, column=0)
+        ttk.Label(self, text='Encoded:').grid(row=0, column=1)
+        self.encoded_in = CompleteText(self, width=80, height=8)
+        self.encoded_in.grid(row=1, column=1)
+        self.plain_out = CompleteText(self)
+        self.plain_out.grid(row=2, column=1)
+        self.plain_in.bind('<Key>', self.manage_encryption)
+        self.encoded_in.bind('<Key>', self.manage_encryption)
+    def manage_encryption(self, _=None):
+        encoded_out = ''.join(self.me.encode(self.plain_in.get('1.0', 'end-1c').encode()))
+        try:
+            encoded_in = self.encoded_in.get('1.0', 'end-1c').strip()
+            plain_out = self.me.decode(encoded_in)
+        except Exception as error:
+            plain_out = '%s %s' % (error, error.with_traceback(None))
+        self.encoded_out.replace('1.0', 'end-1c', encoded_out)
+        self.plain_out.replace('1.0', 'end-1c', plain_out)
+        
+
+gui = GUI()
+gui.grid()
+gui.focus_set()
+gui.mainloop()
--- markov.py
+++ markov.py
@@ -8,8 +8,13 @@ class MarkovChain():
         self._data = {}
 
     def add(self, sequence):
-        for i, item in enumerate(sequence):
+        for i, item in enumerate(sequence[:self._state_len]):
             key = tuple(sequence[max(0, i-self._state_len):i])
+            data = self._data.get(key, dict())
+            data[item] = data.get(item, 0)+1
+            self._data[key] = data
+        for i, item in enumerate(sequence[self._state_len:]):
+            key = tuple(sequence[i:i+self._state_len])
             data = self._data.get(key, dict())
             data[item] = data.get(item, 0)+1
             self._data[key] = data
@@ -23,7 +28,7 @@ class MarkovChain():
 
 
 class MarkovEncode():
-    def __init__(self, s_list, state_len=2):
+    def __init__(self, s_list, state_len=10):
         chain = MarkovChain(state_len=state_len)
         for sequence in s_list:
             chain.add(sequence)
@@ -31,8 +36,8 @@ class MarkovEncode():
 
     def encode(self, data):
         chain = self._chain
-        data = base64.b64encode(data)
-        bits = ''.join(('0'*7+bin(char)[2:])[-7:] for char in data)
+        #data = base64.b64encode(data)
+        bits = ''.join(('0'*8+bin(char)[2:])[-8:] for char in data)
         current = []
         encoded = []
         for bit in bits:
@@ -64,19 +69,49 @@ class MarkovEncode():
                 else:
                     current.append(item)
                     break
-        length = int(len(bits)/7)
-        chars = (bits[i*7:i*7+7] for i in range(length))
+        length = int(len(bits)/8)
+        chars = (bits[i*8:i*8+8] for i in range(length))
         decoded = bytes(int(''.join(map(str, char)),2) for char in chars)
-        return base64.b64decode(decoded)
+        return decoded#base64.b64decode(decoded)
+class MarkovEncode2():
+    def __init__(self, s_list, state_len=10):
+        chain = MarkovChain(state_len=state_len)
+        for sequence in s_list:
+            chain.add(sequence)
+        self._chain = chain
+    def encode(self, data):
+        chain = self._chain
+        bits = ['0'*8+bin(char)[2:])[-8:] for char in data]
+        current = []
+        encoded = []
+        while bits:
+            options = chain.next(current)
+            if options:
+                bits_to_use = int(math.log(len(options), 2))
+                current_bits = 0
+                for i in range(1, bits_to_use + 1):
+                    current_bits += int(bits.pop(0)) * 2**i
+                current += options[current_bits]
+            else:
+                encoded.extend(current)
+                current = []
+        encoded.extend(current)
+        return encoded
+    def decode(self, data):
+        chain = self._chain
+        bits = []
+        current = data.copy()
+        simulated = []
+        while current:
+            options = chain.next(simulated)
+            simulated += current[0]
+            if options:
+                current_bits = options.index(current)
+        
 
 def shakespeare():
     import json
     with open('shakespeare.json') as file:
         data = json.load(file)
-    return [i.split(' ') for i in data]
-SHAKESPEARE = shakespeare()
-ME = MarkovEncode(SHAKESPEARE)
-encoded = ME.encode(b'Hi there'+bytes(range(100)))
-print(' '.join(encoded))
-decoded = ME.decode(encoded)
-print(decoded)
+    return data
+
